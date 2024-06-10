fe75ff36fef234a5c9ea327e6d6c3bf76bbe409b
saraauc <s.mohamed2209@gmail.com>
2024-06-10 07:46:01 -0400

    This code aims to add a subfolder called migration

---
+++ create_subfolder.py
@@ -0,0 +1,9 @@
+import os
+
+# Get a list of all directories in the current directory
+directories = [d for d in os.listdir('.') if os.path.isdir(d)]
+
+# Create the 'migration' subfolder in each directory
+for dir in directories:
+    migration_path = os.path.join(dir, 'migration')
+    os.makedirs(migration_path, exist_ok=True)
